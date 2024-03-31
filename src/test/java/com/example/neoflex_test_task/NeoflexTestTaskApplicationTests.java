ced718b27a722e77a7d25e47174a91c69e533aa2
Stepan Bogdanov <bogstepan@gmail.com>
2024-03-31 23:21:55 +0300

    Initial commit

---
+++ src/test/java/com/example/neoflex_test_task/NeoflexTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neoflex_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
